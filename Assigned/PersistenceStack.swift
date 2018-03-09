7cdc8211e17c2f44529ebffd32f411e808df8031
LinnierGames <esericksanc@gmail.com>
2018-03-09 11:21:08 -0800

    PersistanceStack

---
+++ Assigned/PersistenceStack.swift
@@ -0,0 +1,58 @@
+//
+//  PeresistanceStack.swift
+//  Assigned
+//
+//  Created by Erick Sanchez on 3/9/18.
+//  Copyright © 2018 LinnierGames. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+struct PersistenceStack {
+    
+    static var shared = PersistenceStack()
+    
+    // MARK: - Core Data stack
+    
+    var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Assigned")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                
+                /*
+                 Typical reasons for an error here include:
+                 * The parent directory does not exist, cannot be created, or disallows writing.
+                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                 * The device is out of space.
+                 * The store could not be migrated to the current model version.
+                 Check the error message to determine what the actual problem was.
+                 */
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    var viewContext: NSManagedObjectContext {
+        return self.persistentContainer.viewContext
+    }
+    
+    // MARK: - Core Data Saving support
+    
+    func saveContext () {
+        let context = self.persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
