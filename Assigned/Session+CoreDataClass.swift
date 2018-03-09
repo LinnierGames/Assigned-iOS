cfae7c7a6b6d47434e8547da0c0415218c31872e
LinnierGames <esericksanc@gmail.com>
2018-03-09 10:54:05 -0800

    Task MO

---
+++ Assigned/Session+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Session+CoreDataClass.swift
+//  Assigned
+//
+//  Created by Erick Sanchez on 3/9/18.
+//  Copyright © 2018 LinnierGames. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Session)
+public class Session: NSManagedObject {
+
+}
---
+++ Assigned/Task+CoreDataClass.swift
@@ -0,0 +1,67 @@
+//
+//  Task+CoreDataClass.swift
+//  Assigned
+//
+//  Created by Erick Sanchez on 3/9/18.
+//  Copyright © 2018 LinnierGames. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Task)
+public class Task: NSManagedObject {
+    
+    enum Priorities: Int, Equatable, CustomStringConvertible {
+        case None = 0
+        case Low
+        case Medium
+        case High
+        
+        var description: String {
+            switch self {
+            case .None:
+                return ""
+            case .Low:
+                return "Low"
+            case .Medium:
+                return "Medium"
+            case .High:
+                return "High"
+            }
+        }
+    }
+    
+    /** public interface of the task priority */
+    var priority: Priorities {
+        set {
+            self.priorityValue = Int16(newValue.rawValue)
+        }
+        
+        get {
+            guard let priority = Priorities(rawValue: Int(self.priorityValue)) else {
+                fatalError("could not convert into an enum from database")
+            }
+            
+            return priority
+        }
+    }
+    
+    convenience init(title: String, effort: Float,
+                     deadline: Date? = nil,
+                     priority: Task.Priorities = .None,
+                     notes: String = "",
+                     isCompleted: Bool = false,
+                     in context: NSManagedObjectContext) {
+        self.init(context: context)
+        
+        self.title = title
+        self.effort = effort
+        self.deadline = deadline
+        self.priorityValue = Int16(priority.rawValue)
+        self.notes = notes
+        self.isCompleted = isCompleted
+    }
+
+}
