0bc994d395ed97b3402c301e0de02f3537927c70
LinnierGames <esericksanc@gmail.com>
2018-03-09 11:12:43 -0800

    Session MO and Task relationships

--- Assigned/Session+CoreDataClass.swift
+++ Assigned/Session+CoreDataClass.swift
@@ -12,5 +12,18 @@ import CoreData
 
 @objc(Session)
 public class Session: NSManagedObject {
-
+    
+    convenience init(name: String,
+                     date: Date,
+                     duration: TimeInterval,
+                     task: Task? = nil,
+                     in context: NSManagedObjectContext) {
+        self.init(context: context)
+        
+        self.name = name
+        self.date = date
+        self.duration = duration
+        
+        self.task = task
+    }
 }
