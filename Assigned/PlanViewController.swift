90bff4677349cf3d700362c1490204dfb1060bf6
LinnierGames <esericksanc@gmail.com>
2018-04-03 09:32:46 -0700

    Layout Task Panel

--- Assigned/PlanViewController.swift
+++ Assigned/PlanViewController.swift
@@ -9,11 +9,29 @@
 import UIKit
 
 class PlanViewController: UIViewController {
-
     
     // MARK: - RETURN VALUES
     
     // MARK: - VOID METHODS
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let identifier = segue.identifier {
+            switch identifier {
+            case "embed task panel":
+                guard let vc = segue.destination as? TaskPanelViewController else {
+                    fatalError("TaskPanelViewController was not set correctly in the storyboard")
+                }
+                
+                let panGesture = UIPanGestureRecognizer(target: self, action: #selector(PlanViewController.didPanTaskPanel(_:)))
+                vc.view.addGestureRecognizer(panGesture)
+            default: break
+            }
+        }
+    }
+    
+    @objc private func didPanTaskPanel(_ sender: Any) {
+        print("AAAAAA")
+    }
     
     // MARK: - IBACTIONS
     
---
+++ Assigned/TaskPanelViewController.swift
@@ -0,0 +1,22 @@
+//
+//  TaskPanelViewController.swift
+//  Assigned
+//
+//  Created by Erick Sanchez on 4/3/18.
+//  Copyright © 2018 LinnierGames. All rights reserved.
+//
+
+import UIKit
+
+class TaskPanelViewController: UIViewController {
+
+    
+    // MARK: - RETURN VALUES
+    
+    // MARK: - VOID METHODS
+    
+    // MARK: - IBACTIONS
+    
+    // MARK: - LIFE CYCLE
+
+}
