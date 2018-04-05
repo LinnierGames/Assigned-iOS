23a8d94f2f626307020fb532c94ca64651783b85
LinnierGames <esericksanc@gmail.com>
2018-04-04 19:31:49 -0700

    Populate Events for Today's Date

--- Assigned/CalendarTableViewController.swift
+++ Assigned/CalendarTableViewController.swift
@@ -22,14 +22,18 @@ class CalendarTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return calendars?.count ?? 0
+        return events?.count ?? 0
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         
-        let calendar = calendars![indexPath.row]
-        cell.textLabel!.text = calendar.title
+        let event = events![indexPath.row]
+        cell.textLabel!.text = event.title
+        
+        let startTime = String(date: event.startDate, dateStyle: .none, timeStyle: .short)
+        let endTime = String(date: event.endDate, dateStyle: .none, timeStyle: .short)
+        cell.detailTextLabel!.text = "\(startTime) - \(endTime)"
         
         return cell
     }
@@ -39,6 +43,29 @@ class CalendarTableViewController: UITableViewController {
     private
     func loadCalendars() {
         self.calendars = eventStore.calendars(for: EKEntityType.event)
+    }
+    
+    var selectedCalendar: EKCalendar! // Passed in from previous view controller
+    var events: [EKEvent]?
+    
+    func loadEvents() {
+        
+        // Create start and end date NSDate instances to build a predicate for which events to select
+        let startDate = Date().midnight
+        let endDate = Date().endOfDay
+        
+        if let calendars = self.calendars {
+            let eventStore = EKEventStore()
+            
+            // Use an event store instance to create and properly configure an NSPredicate
+            let eventsPredicate = eventStore.predicateForEvents(withStart: startDate, end: endDate, calendars: calendars)
+            
+            // Use the configured NSPredicate to find and return events in the store that match
+            self.events = eventStore.events(matching: eventsPredicate).sorted() {
+                (e1: EKEvent, e2: EKEvent) -> Bool in
+                return e1.startDate.compare(e2.startDate) == ComparisonResult.orderedAscending
+            }
+        }
     }
     
     private
@@ -60,6 +87,7 @@ class CalendarTableViewController: UITableViewController {
         case EKAuthorizationStatus.authorized:
             // Things are in line with being able to show the calendars in the table view
             loadCalendars()
+            loadEvents()
             refreshTableView()
         case EKAuthorizationStatus.restricted, EKAuthorizationStatus.denied:
             // We need to help them give us permission
