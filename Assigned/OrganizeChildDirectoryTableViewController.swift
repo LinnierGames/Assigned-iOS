bdb3addeee0b60b22c0558422ab9751d01fde056
LinnierGames <esericksanc@gmail.com>
2018-04-12 19:41:57 -0700

    init abstraction classes of OrganizeTableViewController

---
+++ Assigned/OrganizeChildDirectoryTableViewController.swift
@@ -0,0 +1,21 @@
+//
+//  OrganizeChildDirectoryTableViewController.swift
+//  Assigned
+//
+//  Created by Erick Sanchez on 4/12/18.
+//  Copyright © 2018 LinnierGames. All rights reserved.
+//
+
+import UIKit
+
+class OrganizeChildDirectoryTableViewController: OrganizeTableViewController {
+
+    // MARK: - RETURN VALUES
+    
+    // MARK: - VOID METHODS
+    
+    // MARK: - IBACTIONS
+    
+    // MARK: - LIFE CYCLE
+
+}
---
+++ Assigned/OrganizeSmartFolderTableViewController.swift
@@ -0,0 +1,21 @@
+//
+//  OrganizeSmartFolderTableViewController.swift
+//  Assigned
+//
+//  Created by Erick Sanchez on 4/12/18.
+//  Copyright © 2018 LinnierGames. All rights reserved.
+//
+
+import UIKit
+
+class OrganizeSmartFolderTableViewController: OrganizeTableViewController {
+
+    // MARK: - RETURN VALUES
+    
+    // MARK: - VOID METHODS
+    
+    // MARK: - IBACTIONS
+    
+    // MARK: - LIFE CYCLE
+
+}
